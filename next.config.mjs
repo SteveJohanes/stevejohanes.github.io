73e4eea240b68ba3f71d801577ddae266a46c55c
steve <118546674+SteveJohanes@users.noreply.github.com>
2026-06-10 16:20:34 +0700

    setup github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
+  images: {
+    unoptimized: true,
+  },
   trailingSlash: true,
 };
 
